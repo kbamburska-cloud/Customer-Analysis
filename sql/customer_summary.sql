4e59bd753659c81c206e3cce0db2dc2392f4dccd
Kat Bamburska <k.bamburska@gmail.com>
2026-04-12 16:37:04 +0100

    Update customer_summary.sql

--- sql/customer_summary.sql
+++ sql/customer_summary.sql
@@ -4,7 +4,7 @@ cu.last_name,
 count (distinct p.policy_id) as total_policies,
 sum(p.premium) as total_premium,
 sum(c.claim_amount) as total_claims,
-round(sum(c.claim_amount)*100.0/sum(p.premium),2) as loss_ratio_pct
+round(sum(c.claim_amount)*1.0/sum(p.premium),2) as loss_ratio
 from customers cu
 left join policies p 
 on cu.customer_id =p.customer_id 
