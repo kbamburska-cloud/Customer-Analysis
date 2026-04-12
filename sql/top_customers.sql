7d12c252986bcfee2b35dc0b2a6a889a9ff52a00
Kat Bamburska <k.bamburska@gmail.com>
2026-04-12 18:34:11 +0100

    Create top_customers.sql

---
+++ sql/top_customers.sql
@@ -0,0 +1,18 @@
+with customer_summary as(
+select cu.customer_id,
+cu.first_name,
+cu.last_name,
+count (distinct p.policy_id) as total_policies,
+sum(p.premium) as total_premium,
+sum(c.claim_amount) as total_claims,
+round(sum(c.claim_amount)*1.0/sum(p.premium),2) as loss_ratio
+from customers cu
+left join policies p 
+on cu.customer_id =p.customer_id 
+left join claims c 
+on p.policy_id = c.policy_id 
+group by cu.customer_id, cu.first_name, cu.last_name)
+select *,
+rank() over(order by total_premium desc) as rank_by_premium,
+rank() over(order by loss_ratio desc) as rank_by_risk
+from customer_summary;
